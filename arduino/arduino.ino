ae738b0537e0fd78dc87264cca5fd3e9fe461c60
Noé <eon.sberk@gmail.com>
2023-09-05 19:39:38 +0200

    test

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,21 +1,30 @@
-void setup() {
+void setup()
+{
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
+void loop()
+{
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // print out the value you read:
-  
-  if (Serial.available() > 0) {
+
+  if (Serial.available() > 0)
+  {
     // read the incoming byte:
     int incomingByte = Serial.read();
     // say what you got:
+    char byteString[10];
+
+    // Convert the incoming byte to a string
+    snprintf(byteString, sizeof(byteString), "%d", incomingByte);
+
+    // say what you got as a string:
     Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    Serial.println(byteString);
   }
 
-  delay(1);  // delay in between reads for stability
+  delay(1); // delay in between reads for stability
 }
