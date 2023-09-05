9e8d2b90004c17119b0f0755d63097b02628a6bb
Noé <eon.sberk@gmail.com>
2023-09-05 19:34:36 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [port, setPort] = useState<SerialPort | null>(null);
-  const listen = async (reader: ReadableStreamDefaultReader<string>) => {
+
+  const listen = async (
+    reader:
+      | ReadableStreamDefaultReader<string>
+      | ReadableStreamDefaultReader<Uint8Array>
+  ) => {
     while (true) {
       const { value, done } = await reader.read();
       if (done) {
@@ -17,16 +22,13 @@ export default function Home() {
       console.log(value);
     }
   };
-  const getDevices = async () => {
+  const connect = async () => {
     const filters: SerialPortRequestOptions["filters"] = [];
 
     // Prompt user to select an Arduino Uno device.
     const port = await navigator.serial.requestPort({ filters });
 
     setPort(port);
-
-    const { usbProductId, usbVendorId } = port.getInfo();
-
     // await port.close();
 
     await port.open({ baudRate: 9600 });
@@ -36,25 +38,6 @@ export default function Home() {
     const reader = textDecoder.readable.getReader();
 
     if (reader) listen(reader);
-
-    const encoder = new TextEncoder();
-
-    // Send "Hello, World!" to the serial port
-    const message = "Hello, World!";
-    const data = encoder.encode(message);
-
-    const writer = port.writable?.getWriter();
-
-    if (!writer) return;
-
-    await writer.write(data);
-
-    console.log(`Sent: ${message}`);
-
-    // Close the port when you're done
-    // await port.close();
-
-    console.log(usbProductId, usbVendorId);
   };
 
   const send = async () => {
@@ -71,6 +54,8 @@ export default function Home() {
     await writer.write(data);
 
     console.log(`Sent: ${message}`);
+
+    writer.releaseLock();
   };
 
   useEffect(() => {
@@ -94,14 +79,16 @@ export default function Home() {
   return (
     <main className="flex gap-2 p-24">
       <button
-        className="bg-orange-400 text-black font-bold rounded-lg px-3 py-2"
-        onClick={getDevices}
+        className="bg-orange-400 text-black font-bold rounded-lg px-3 py-2 disabled:opacity-50"
+        onClick={connect}
+        disabled={!!port}
       >
         Connect
       </button>
       <button
-        className="bg-orange-400 text-black font-bold rounded-lg px-3 py-2"
+        className="bg-orange-400 text-black font-bold rounded-lg px-3 py-2  disabled:opacity-50"
         onClick={send}
+        disabled={!port}
       >
         Send IT
       </button>
