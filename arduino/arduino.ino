a6da2a0041dd15da00b8a2ea2a35af23c4b38c34
Noé <eon.sberk@gmail.com>
2023-09-06 10:50:23 +0200

    communication working

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,24 +10,34 @@ void setup() {
 
 void loop() {
   input = "";
+  bool startMarkerFound = false; // Flag to track whether the start marker is found
 
   // Wait for the start marker
-  while (Serial.available() && Serial.read() != PKG_START);
-
-  // Read the input until the end marker or maximum length is reached
-  char currentInput;
-  unsigned long startTime = millis();
-  while (millis() - startTime < 1000) { // Timeout after 1 second
-    if (Serial.available()) {
-      currentInput = Serial.read();
-      if (currentInput == PKG_END || input.length() >= MAXIMUM_INPUT_LENGTH) {
-        break; // Exit the loop when the end marker is found or max length reached
-      }
-      input += currentInput;
+  while (Serial.available()) {
+    char currentChar = Serial.read();
+    if (currentChar == PKG_START) {
+      startMarkerFound = true;
+      break;
     }
   }
 
-  // Print the received input
-  Serial.print("Received input: ");
-  Serial.println(input);
+  // Continue reading if the start marker is found
+  if (startMarkerFound) {
+    // Read the input until the end marker or maximum length is reached
+    char currentInput;
+    unsigned long startTime = millis();
+    while (millis() - startTime < 1000) { // Timeout after 1 second
+      if (Serial.available()) {
+        currentInput = Serial.read();
+        if (currentInput == PKG_END || input.length() >= MAXIMUM_INPUT_LENGTH) {
+          break; // Exit the loop when the end marker is found or max length reached
+        }
+        input += currentInput;
+      }
+    }
+
+    // Print the received input
+    Serial.print("Received input: ");
+    Serial.println(input);
+  }
 }
