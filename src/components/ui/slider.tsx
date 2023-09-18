2fc0c598a4e07d845a53a4cfb94315f67dd89b10
Tim Reber <tim.reber.1@postfinance.ch>
2023-09-18 11:34:42 +0200

    chore: slider not optimal

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -12,17 +12,18 @@ const Slider = React.forwardRef<
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      `relative flex w-full touch-none select-none items-center ${props.orientation=="vertical" ? "flex flex-col w-20 h-96" : ""}`,
       className
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className={cn("absolute h-full bg-primary")} />
+    <SliderPrimitive.Track className={`relative h-2 w-full grow overflow-hidden rounded-full bg-secondary ${props.orientation=="vertical" ? "w-3":""}`}>
+      <SliderPrimitive.Range className={cn(`absolute h-full bg-primary ${props.orientation=="vertical" ? "w-full":""}`)}/>
     </SliderPrimitive.Track>
     {props.defaultValue?.map((value, i) => {
       return (
         <SliderPrimitive.Thumb
+          aria-orientation={props.orientation == "vertical" ? "vertical" : "horizontal"}
           key={i}
           className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
         >
